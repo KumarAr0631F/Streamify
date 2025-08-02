43636ead7d3a427815fb462992bef293a712836d
KumarAr0631F <nitin.kumar132005@gmail.com>
2025-08-02 16:25:10 +0530

    protecting my env file

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -113,5 +113,49 @@ export async function logout(req, res) {
 }
 
 export async function onboard(req, res){
-    
+    try {
+       const userId = req.user._id;
+       const {fullName, bio, nativeLanguage, learningLanguage, location} = req.body;
+       
+       if(!fullName || !bio || !nativeLanguage || !learningLanguage || !location){
+            return res.status(400).json({
+                message: "All fields are required for onboarding",
+                missingFields: [
+                    !fullName && "fullName",
+                    !bio && "bio",
+                    !nativeLanguage && "nativeLanguage",
+                    !learningLanguage && "learningLanguage",
+                    !location && "location",
+                ].filter(Boolean),
+            });
+       }
+
+       const updatedUser = await User.findByIdAndUpdate(userId, {
+        ...req.body,
+        isOnboarded: true,
+         }, {new: true});
+
+         if(!updatedUser){
+            return res.status(404).json({message: "User not found"});
+         }
+
+         try {
+            await upsertStreamUser({
+            id: updatedUser._id.toString(),
+            name: updatedUser.fullName,
+            image: updatedUser.profilePic || "",
+         })
+         console.log(`Stream user is updated for ${updatedUser.fullName}`);
+
+        
+         } catch (error) {
+            console.error("Error updating Stream user:", error);
+         }
+
+         res.status(200).json({success: true, user: updatedUser});
+
+    } catch (error) {
+        console.error("Error in onboard controller", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
