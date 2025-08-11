ba618566764151d51b44152f23a45b52361b223a
KumarAr0631F <nitin.kumar132005@gmail.com>
2025-08-11 20:39:35 +0530

    update server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,6 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
 }))
-
 app.use(express.json());
 app.use(cookieParser());
 
@@ -27,7 +26,7 @@ if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+        res.sendFile(path.join(__dirname, '../frontend', "dist", "index.html"));
     });
 }
 
