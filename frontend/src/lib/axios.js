1b7bc5c75f00987d0f0de20fa130a4c93d893528
KumarAr0631F <nitin.kumar132005@gmail.com>
2025-08-11 21:05:18 +0530

    upadte the axios file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://streamify-backend-27qx.onrender.com/api" : "/api";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
