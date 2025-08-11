1b76ea5f70b4005948df819c8d820e4201365b94
KumarAr0631F <nitin.kumar132005@gmail.com>
2025-08-11 20:34:25 +0530

    update env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,6 +19,7 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
 }))
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -26,7 +27,7 @@ if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend', "dist", "index.html"));
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
     });
 }
 
